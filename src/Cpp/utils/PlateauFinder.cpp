37adb9a8679010364f0dda689f73ebf764261554
Bizzzio <fachi2001@gmail.com>
2023-04-28 20:14:37 +0200

    Added comments to make my boss happy :)

--- src/Cpp/utils/PlateauFinder.cpp
+++ src/Cpp/utils/PlateauFinder.cpp
@@ -1,4 +1,4 @@
-// script che aiuta a capire fin dove si ha un andamento lineare
+// Script for finding a linear plateau
 
 #include "PlateauFinder.h"
 
@@ -33,28 +33,41 @@ fHisto(histo), fOutname(outname), fDirection(direction), fXmax(xmax), fXmin(xmin
     fCanvas->SetFillColor(0);
 }
 
+/**
+ * @brief Function that runs the "algorithm" for finding the plateau
+ */
 void PlateauFinder::FindPlateau()
 {
     for (int i = 2; i <= fNpoints; i++)
     {
+        // Creates fitting function and adds it to fFits
         if (fDirection == "up")
             fFits.push_back(new TF1((std::string("function")+std::to_string(i)).c_str(), "[0]+[1]*x", fXmin, fHisto->GetBinCenter(i)));
 
         else if (fDirection == "down")
             fFits.push_back(new TF1((std::string("function")+std::to_string(i)).c_str(), "[0]+[1]*x", fHisto->GetBinCenter(fNpoints-i+1), fXmax));
         
+        // Opens pad
         fCanvas->cd(i - 1);
 
         TH1D* ClonedHisto = (TH1D*)(fHisto->DrawCopy("",""));
+        ClonedHisto -> SetStats(0);
         ClonedHisto -> Fit(fFits.back(),"RQ");
 
-        std::cout << "Chi^2:" << fFits.back()->GetChisquare() << ", number of DoF: " << fFits.back()->GetNDF() <<
+        std::cout << "Chi^2:" << std::fixed << std::setprecision(5) <<fFits.back()->GetChisquare() << ", number of DoF: " << fFits.back()->GetNDF() <<
                 ", Chi^2 @ confidence level 5%: "<< ROOT::Math::chisquared_quantile_c(0.05,fFits.back()->GetNDF());
         
-        if (fFits.back()->GetChisquare() <= ROOT::Math::chisquared_quantile_c(0.05,fFits.back()->GetNDF()))
+        // Checks whether the chi^2 test succedeed
+        if ((fFits.back()->GetChisquare() - ROOT::Math::chisquared_quantile_c(0.05,fFits.back()->GetNDF())) < 1e-6)
+        {
             std::cout<<"\t-> PASSED";
+            fPassed.push_back(true);
+        }
         else 
+        {
             std::cout<<"\t-> FAILED";
+            fPassed.push_back(false);
+        }
 
         std::cout<< "." << std::endl;
         std::cout << "--------------------------------------------------------------------------------------------------------" << std::endl;
@@ -76,4 +89,28 @@ TH1D* PlateauFinder::FakeHistoforTest()
         histo->SetBinError(i+1,sigy[i]);
     }
     return histo;
+}
+
+/**
+ * @brief Function that returns the x position of the first point at which the chi^2 test failed
+ * 
+ * @return double 
+ */
+double PlateauFinder::GetxFirstFailed()
+{
+    for (unsigned i=0; i<fPassed.size(); i++)
+    {
+        if (!fPassed[i])
+        {
+            if (fDirection=="up")
+                // if fPassed[0] is false, then test failed when considering the 1st+2nd bins -> i+2
+                return fHisto->GetBinCenter(i+2);
+            else
+                // if fPassed[0] is false, then test failed when considering the fNpoints-th+(fNpoints-1)-th bins -> fNpoints-1-i
+                return fHisto->GetBinCenter(fNpoints-1-i);
+        }
+
+    }
+    std::cout<<"\033[93mThe test hasn't failed, returning 0\033[0m"<<std::endl;
+    return 0;
 }
