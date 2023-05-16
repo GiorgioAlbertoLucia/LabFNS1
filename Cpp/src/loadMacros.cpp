5b526ae6ec7039a8a611a4a665e96fcaab771177
Bizzzio <fachi2001@gmail.com>
2023-05-16 09:12:30 +0200

    runs Run

--- Cpp/src/loadMacros.cpp
+++ Cpp/src/loadMacros.cpp
@@ -14,9 +14,15 @@ void loadMacros(TString myopt="")
     // clean build directory
     if (myopt.Contains("clean"))    gSystem->Exec("bash clean.sh");
 
-    //gSystem->CompileMacro("Cpp/yaml/Yaml.cpp",opt.Data(),"","Cpp/build");
+    gSystem->CompileMacro("Cpp/yaml/Yaml.cpp",opt.Data(),"","Cpp/build");
+    //gSystem->CompileMacro("Cpp/src/hello.cpp",opt.Data(),"","Cpp/build");
     gSystem->CompileMacro("Cpp/utils/newDumper.cpp",opt.Data(),"","Cpp/build");
     //gSystem->CompileMacro("Cpp/utils/fileManager.cpp",opt.Data(),"","Cpp/build");
     gSystem->CompileMacro("Cpp/src/Module.cpp",opt.Data(),"","Cpp/build");
     gSystem->CompileMacro("Cpp/src/Event.cpp",opt.Data(),"","Cpp/build");
+    gSystem->CompileMacro("Cpp/src/Run.cpp",opt.Data(),"","Cpp/build");
+    //gROOT->ProcessLine(".x /home/fabrizio/Documents/Lectures/Lab1/LabFNS1/Cpp/src/event_test.cpp");
+    //gROOT->ProcessLine("NewDumper dumpy(\"data/input/example_pedestal.dat\");");
+    //gROOT->ProcessLine("Event b(0, \"Cpp/configs/config_Events2.yml\", dumpy);");
+    gROOT->ProcessLine("Run a(\"Cpp/configs/config_Events2.yml\");");
 }
