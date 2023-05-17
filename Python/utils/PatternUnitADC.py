50c706ca337295324560bf0e155ede7d7f4e0ac7
Bizzzio <fachi2001@gmail.com>
2023-05-17 15:06:02 +0200

    Final plots

--- Python/utils/PatternUnitADC.py
+++ Python/utils/PatternUnitADC.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import uproot
-from ROOT import TH1D, TCanvas, kAzure, kRed, TLegend, TLatex, gStyle
+from ROOT import TH1D, TCanvas, kAzure, kRed, kGreen, kSpring, TLegend, TLatex, gStyle
 
 from StyleFormatter import SetGlobalStyle, SetObjectStyle
 
@@ -17,15 +17,15 @@ Df=Tree.arrays(library='pd')
 
 Df=Df[["Module4_10","Module4_11","Module5_0"]]
 DfPassed=Df.query("Module5_0==1")
+DfNotPassed=Df.query("Module5_0==0")
 
 cS1 = TCanvas("S1","S1",1500,1500)
-hFrame = cS1.DrawFrame(Df["Module4_10"].min()*0.9,0.0001,Df["Module4_10"].max()*1.02,0.01,"S1 Normalised counts; Channel; Counts")
+hFrame = cS1.DrawFrame(Df["Module4_10"].min()*0.9,0.0001,Df["Module4_10"].max()*1.02,0.01,"ADC Normalised counts; Channel; Counts")
 hS1All = TH1D("hS1All","hS1All",2048,0,2048)
 hS1All.FillN(len(Df["Module4_10"]),np.asarray(Df["Module4_10"],'d'),np.asarray([1]*len(Df["Module4_10"]),'d'))
 hS1All.Scale(1/len(Df))
 hS1All.Rebin(2)
 SetObjectStyle(hS1All,color=kAzure+3,fillalpha=0.9,linewidth=1)
-
 hS1All.Draw("hist,same")
 
 hS1Passed = TH1D("hS1Passed","hS1Passed",2048,0,2048)
@@ -40,8 +40,8 @@ leg.SetTextFont(42)
 leg.SetTextSize(gStyle.GetTextSize()*0.7)
 leg.SetFillStyle(0)
 #leg.SetHeader("S1 Normalised counts")
-leg.AddEntry(hS1All, 'Without S2 inhibit', 'lf')
-leg.AddEntry(hS1Passed, 'With S2 inhibit', 'lf')
+leg.AddEntry(hS1All, 'With and without S2 signal', 'lf')
+leg.AddEntry(hS1Passed, 'Without S2 signal', 'lf')
 leg.Draw("same")
 
 text =TLatex(0.45, 0.73,"S1 Scintillator + PMXP2020")
@@ -54,12 +54,12 @@ text2.SetNDC()
 text2.SetTextSize(gStyle.GetTextSize()*0.7)
 text2.SetTextFont(42)
 text2.Draw()
-text3 =TLatex(0.45, 0.49,"Acquisition time: 300 s")
+text3 =TLatex(0.45, 0.49,"Acquisition time: 236985 s")
 text3.SetNDC()
 text3.SetTextSize(gStyle.GetTextSize()*0.7)
 text3.SetTextFont(42)
 text3.Draw()
-text4 =TLatex(0.45, 0.43,"Discriminator threshold value: (-39.6 #pm 0.5) mV")
+text4 =TLatex(0.45, 0.43,"Discriminator threshold value: (#font[122]{-}39.6 #pm 0.5) mV")
 text4.SetNDC()
 text4.SetTextSize(gStyle.GetTextSize()*0.7)
 text4.SetTextFont(42)
@@ -68,6 +68,52 @@ text4.Draw()
 
 cS1.SaveAs('/home/fabrizio/Documents/Lectures/Lab1/LabFNS1/Python/utils/cS1.pdf')
 
+cS1NotPassed = TCanvas("S1","S1",1500,1500)
+hFrame = cS1NotPassed.DrawFrame(Df["Module4_10"].min()*0.9,0.0001,Df["Module4_10"].max()*1.02,0.01,"ADC Normalised counts; Channel; Counts")
+
+hS1All.Draw("hist,same")
+
+hS1NotPassed = TH1D("hS1NotPassed","hS1NotPassed",2048,0,2048)
+hS1NotPassed.FillN(len(DfNotPassed["Module4_10"]),np.asarray(DfNotPassed["Module4_10"],'d'),np.asarray([1]*len(DfNotPassed["Module4_10"]),'d'))
+hS1NotPassed.Scale(1/len(DfNotPassed))
+hS1NotPassed.Rebin(2)
+SetObjectStyle(hS1NotPassed,color=kSpring-5,fillalpha=0.5,linewidth=1)
+hS1NotPassed.Draw("hist,same")
+
+hS1Passed.Draw("hist,same")
+
+leg = TLegend(0.435, 0.71, 0.85, 0.53)
+leg.SetTextFont(42)
+leg.SetTextSize(gStyle.GetTextSize()*0.7)
+leg.SetFillStyle(0)
+#leg.SetHeader("S1 Normalised counts")
+leg.AddEntry(hS1All, 'With and without S2 signal', 'lf')
+leg.AddEntry(hS1Passed, 'Without S2 signal', 'lf')
+leg.AddEntry(hS1NotPassed, 'With S2 signal', 'lf')
+leg.Draw("same")
+
+text =TLatex(0.45, 0.73,"S1 Scintillator + PMXP2020")
+text.SetNDC()
+text.SetTextSize(gStyle.GetTextSize())
+text.SetTextFont(42)
+text.Draw()
+text2 =TLatex(0.45, 0.49,"Using V259 pattern unit")
+text2.SetNDC()
+text2.SetTextSize(gStyle.GetTextSize()*0.7)
+text2.SetTextFont(42)
+text2.Draw()
+text3 =TLatex(0.45, 0.43,"Acquisition time: 236985 s")
+text3.SetNDC()
+text3.SetTextSize(gStyle.GetTextSize()*0.7)
+text3.SetTextFont(42)
+text3.Draw()
+text4 =TLatex(0.45, 0.37,"Discriminator threshold value: (#font[122]{-}39.6 #pm 0.5) mV")
+text4.SetNDC()
+text4.SetTextSize(gStyle.GetTextSize()*0.7)
+text4.SetTextFont(42)
+text4.Draw()
+
+cS1NotPassed.SaveAs('/home/fabrizio/Documents/Lectures/Lab1/LabFNS1/Python/utils/cS1All.pdf')
 
 
 ##########
@@ -75,7 +121,7 @@ cS1.SaveAs('/home/fabrizio/Documents/Lectures/Lab1/LabFNS1/Python/utils/cS1.pdf'
 ##########
 
 cSG = TCanvas("SG","SG",1500,1500)
-hFrame = cSG.DrawFrame(Df["Module4_11"].min()*0.9,0.0001,Df["Module4_11"].max()*1.02,0.006,"SG Normalised counts; Channel; Counts")
+hFrame = cSG.DrawFrame(Df["Module4_11"].min()*0.9,0.0001,Df["Module4_11"].max()*1.02,0.006,"ADC Normalised counts; Channel; Counts")
 hSGAll = TH1D("hSGAll","hSGAll",2048,0,2048)
 hSGAll.FillN(len(Df["Module4_11"]),np.asarray(Df["Module4_11"],'d'),np.asarray([1]*len(Df["Module4_11"]),'d'))
 hSGAll.Scale(1/len(Df))
@@ -84,7 +130,6 @@ SetObjectStyle(hSGAll,color=kAzure+3,fillalpha=0.9,linewidth=1)
 
 hSGAll.Draw("hist,same")
 
-DfPassed=Df.query("Module5_0==1")
 hSGPassed = TH1D("hSGPassed","hSGPassed",2048,0,2048)
 hSGPassed.FillN(len(DfPassed["Module4_11"]),np.asarray(DfPassed["Module4_11"],'d'),np.asarray([1]*len(DfPassed["Module4_11"]),'d'))
 hSGPassed.Scale(1/len(DfPassed))
@@ -97,8 +142,8 @@ leg.SetTextFont(42)
 leg.SetTextSize(gStyle.GetTextSize()*0.7)
 leg.SetFillStyle(0)
 #leg.SetHeader("SG Normalised counts")
-leg.AddEntry(hSGAll, 'Without S2 inhibit', 'lf')
-leg.AddEntry(hSGPassed, 'With S2 inhibit', 'lf')
+leg.AddEntry(hSGAll, 'With and without S2 signal', 'lf')
+leg.AddEntry(hSGPassed, 'Without S2 signal', 'lf')
 leg.Draw("same")
 
 text =TLatex(0.48, 0.83,"SG Scintillator + PM R1513")
@@ -111,12 +156,12 @@ text2.SetNDC()
 text2.SetTextSize(gStyle.GetTextSize()*0.7)
 text2.SetTextFont(42)
 text2.Draw()
-text3 =TLatex(0.48, 0.59,"Acquisition time: 300 s")
+text3 =TLatex(0.48, 0.59,"Acquisition time: 236985 s")
 text3.SetNDC()
 text3.SetTextSize(gStyle.GetTextSize()*0.7)
 text3.SetTextFont(42)
 text3.Draw()
-text4 =TLatex(0.48, 0.53,"Discriminator threshold value: (-10.2 #pm 0.5) mV")
+text4 =TLatex(0.48, 0.53,"Discriminator threshold value: (#font[122]{-}10.2 #pm 0.5) mV")
 text4.SetNDC()
 text4.SetTextSize(gStyle.GetTextSize()*0.7)
 text4.SetTextFont(42)
@@ -124,3 +169,49 @@ text4.Draw()
 
 
 cSG.SaveAs('/home/fabrizio/Documents/Lectures/Lab1/LabFNS1/Python/utils/cSG.pdf')
+
+cSGNotPassed = TCanvas("SG","SG",1500,1500)
+hFrame = cSGNotPassed.DrawFrame(Df["Module4_11"].min()*0.9,0.0001,Df["Module4_11"].max()*1.02,0.006,"ADC Normalised counts; Channel; Counts")
+
+hSGAll.Draw("hist,same")
+
+hSGNotPassed = TH1D("hSGNotPassed","hSGNotPassed",2048,0,2048)
+hSGNotPassed.FillN(len(DfNotPassed["Module4_11"]),np.asarray(DfNotPassed["Module4_11"],'d'),np.asarray([1]*len(DfNotPassed["Module4_11"]),'d'))
+hSGNotPassed.Scale(1/len(DfNotPassed))
+hSGNotPassed.Rebin(2)
+SetObjectStyle(hSGNotPassed,color=kSpring-5,fillalpha=0.5,linewidth=1)
+hSGNotPassed.Draw("hist,same")
+hSGPassed.Draw("hist,same")
+
+leg = TLegend(0.465, 0.81, 0.85, 0.63)
+leg.SetTextFont(42)
+leg.SetTextSize(gStyle.GetTextSize()*0.7)
+leg.SetFillStyle(0)
+#leg.SetHeader("SG Normalised counts")
+leg.AddEntry(hSGAll, 'With and without S2 signal', 'lf')
+leg.AddEntry(hSGPassed, 'Without S2 signal', 'lf')
+leg.AddEntry(hSGNotPassed, 'With S2 signal', 'lf')
+leg.Draw("same")
+
+text =TLatex(0.48, 0.83,"SG Scintillator + PM R1513")
+text.SetNDC()
+text.SetTextSize(gStyle.GetTextSize())
+text.SetTextFont(42)
+text.Draw()
+text2 =TLatex(0.48, 0.59,"Using V259 pattern unit")
+text2.SetNDC()
+text2.SetTextSize(gStyle.GetTextSize()*0.7)
+text2.SetTextFont(42)
+text2.Draw()
+text3 =TLatex(0.48, 0.53,"Acquisition time: 236985 s")
+text3.SetNDC()
+text3.SetTextSize(gStyle.GetTextSize()*0.7)
+text3.SetTextFont(42)
+text3.Draw()
+text4 =TLatex(0.48, 0.47,"Discriminator threshold value: (#font[122]{-}10.2 #pm 0.5) mV")
+text4.SetNDC()
+text4.SetTextSize(gStyle.GetTextSize()*0.7)
+text4.SetTextFont(42)
+text4.Draw()
+
+cSGNotPassed.SaveAs('/home/fabrizio/Documents/Lectures/Lab1/LabFNS1/Python/utils/cSGAll.pdf')
