406e5cffa1d2b55d9962a73b305d187d0f7adff8
Marcellocosti <marcellodicostanzo00@gmail.com>
2023-06-05 02:20:20 +0200

    caliberr

--- Python/utils/ReadMCA.py
+++ Python/utils/ReadMCA.py
@@ -5,7 +5,7 @@ import math
 sys.path.append('Python/utils')
 
 from StyleFormatter import SetObjectStyle
-from ROOT import TH1D, TCanvas, kBlue, kGreen, kRed, kBlue, kOrange, kBlack, kAzure, kMagenta, kViolet
+from ROOT import TH1D, TCanvas, kBlue, kGreen, kRed, kBlue, kOrange, kBlack, kAzure, kMagenta, kViolet, Math
 
 def GetPandas(infile):
     df = pd.read_csv(infile, '\n', header = None, engine='python')
@@ -34,26 +34,40 @@ def CreateHist(infile,number,histtitle="",energyscale="chn"):
         return EnergyHisto(hist,df[0],5)
     return hist
 
+
 def EnergyHisto(histo, dataarray, gain):
-    a = 0.3206
-    sa = 0.0014
-    b = -47
-    sb = 7
+    a = 0.318
+    sa = 0.008
+    b = -30
+    sb = 40
     EnHist = TH1D(histo.GetTitle(),histo.GetTitle(),len(dataarray),ChEnConv(0,a,b,gain),ChEnConv(len(dataarray),a,b,gain))
     EnbinEntries = [ChEnConv(bin,a,b,gain) for bin in range(histo.GetNbinsX())]
     EnHist.FillN(len(dataarray),np.asarray(EnbinEntries,'d'),np.asarray(dataarray,'d'))
-    CalErr = CalEnergyErr(a,sa,b,sb,gain)
-    for binnumber in range(EnHist.GetNbinsX()):
-        EnHist.SetBinError(binnumber,np.sqrt(EnHist.GetBinContent(binnumber) + CalErr*CalErr)) 
+    #CalErr = CalEnergyErr(a,sa,b,sb,gain)
+    #for binnumber in range(EnHist.GetNbinsX()):
+    #    EnHist.SetBinError(binnumber,np.sqrt(EnHist.GetBinContent(binnumber) + CalErr*CalErr)) 
     #print(EnHist.Integral())
     return EnHist
 
 def ChEnConv(chn,a,b,gain):
     return (chn-b)/(a*gain)
 
-def CalEnergyErr(a,sa,b,sb,gain):
-    sEn = np.sqrt( (sb*sb)/(a*a*gain*gain) + (b*b*sa*sa)/(a*a*a*a*gain*gain) + 2*sa*sb*( (b)/(a*a*a*gain) ) )
+def CalEnergyErr(a,sa,b,sb,gain,cov,en,sen):
+    #+ 2*sa*sb*( (b)/(a*a*a*gain) )
+    ch = (0.3176)*gain*en - 33.26
+    print('CANALEEEEEEEEEEEEEEEEE')
+    print(ch)
+    print('\n\n\n\n\n')
+    sch = np.sqrt( (0.3176*0.3176*gain*gain*sen*sen) + (en*en*gain*gain*sa*sa) + sb*sb)
+    sEn = np.sqrt( (sb*sb)/(a*a*gain*gain) + ((ch-b)*(ch-b)*sa*sa)/(a*a*a*a*gain*gain) + 2*cov*( (ch-b)/(a*a*a*gain*gain) ) + ((sch*sch)/(a*a*gain*gain)) )
     return sEn
+
+def FitStats(fitfunct):
+    Chisquare = fitfunct.GetChisquare()
+    Chicrit = Math.chisquared_quantile_c(0.05,fitfunct.GetNDF())
+    NDegFreedom = fitfunct.GetNDF()
+    Pvalue = fitfunct.GetProb()
+    print('Chi Square:', Chisquare, ', Degrees of freedom:', NDegFreedom, ', Critical Chi Square:', Chicrit,', P-value: ', Pvalue)
 
 def HistoComparison(histos, legend, canvatags, boundaries, canvapath, rebins=[], colors = [], alphas = []):
     histoscopy = []
@@ -71,35 +85,18 @@ def HistoComparison(histos, legend, canvatags, boundaries, canvapath, rebins=[],
     canva.SaveAs(canvapath)
 
 
-def DictHistos(dict, histofile, rebin=0):
-    colors = [kBlue, kRed, kGreen, kOrange, kBlack, kViolet]
+def DictHistos(dict, histofile, color,rebin=0):
+    #colors = [kBlue, kRed, kGreen, kOrange, kBlack, kViolet]
+    colors = [color, color, color, color, color, color]
     histofile.cd()
     dictkeys = list(dict.keys()) 
     for idx, (dictkey, color) in enumerate(zip(dictkeys,colors)):
-        #if(dictkey == 'Gain 200'): 
-        #print(idx)
         dict[dictkey] = [dict[dictkey],CreateHist(dict[dictkey][0],idx,dictkeys[idx],dict[dictkey][1])]
         if(rebin != 0):
-            print(rebin)
             dict[dictkey][1].Rebin(rebin)
-            #print(0)
-            #print(dictkey)
-        #if(dictkey != 'Gain 200'):
-        #    dict[dictkey] = [dict[dictkey],CreateHist(dict[dictkey],idx,dictkeys[idx],"en1000")]
-        #    #print(1)
-        #    #print(dictkey)
-        #if(dictkey == 'Stronzio'):
-        #    dict[dictkey] = [dict[dictkey],CreateHist(dict[dictkey],idx,dictkeys[idx],"en1000")]
-        ##print(1)
-        ##print(dictkey)
-        #canva = TCanvas('c'+str(idx),'c'+str(idx), 1700, 1200)
-        #dict[dictkey][1].SetLineStyle(1)
-        #print(dict[dictkey])
-        print(dict[dictkey][0][1])
+        dict[dictkey][1].SetLineColor(4)
         SetObjectStyle(dict[dictkey][1], color=color, fillalpha=1, linewidth=1)
-        #dict[dictkey][1].Draw("HIST L")
         dict[dictkey][1].SetDrawOption("E")
-        #dict[dictkey][1].Draw("E")
         dict[dictkey][1].Write()
 
 def Centroid(histo, lowerbound, upperbound):
@@ -128,27 +125,6 @@ def FWHM(histo):
     errFWHM = math.sqrt(sigmabinleft*sigmabinleft + sigmabinright*sigmabinright)
     #errFWHM = math.sqrt(2*maximum + histo.GetBinContent(histo.FindFirstBinAbove(maximum/2,1,1,histo.GetMaximumBin())-1) + histo.GetBinContent(histo.FindLastBinAbove(maximum/2,1,histo.GetMaximumBin(),-1)-1))
     return rightbound-leftbound,errFWHM
-
-#def HistoComparison(histo1, histo2, legend, canvatags, boundaries, canvapath, rebin1=0, rebin2=0):
-#    #histo1.SetNormFactor(1./histo1.Integral())
-#    #histo2.SetNormFactor(1./histo2.Integral())
-#    #histo1.Scale(histo1.GetNormFactor())
-#    #histo2.Scale(histo2.GetNormFactor())
-#    if(rebin1 != 0):
-#        histo1.Rebin(rebin1)
-#    if(rebin2 != 0):
-#        histo1.Rebin(rebin2)
-#    histo1.Scale(1/histo1.GetMaximum())
-#    histo2.Scale(1/histo2.GetMaximum())
-#    #histo1.Scale(1./histo1.Integral("width"))
-#    #histo2.Scale(1./histo2.Integral("width"))
-#    canva = TCanvas('c', 'c', 1700,1200)
-#    hFrame = canva.DrawFrame(boundaries[0], boundaries[1], boundaries[2], boundaries[3], canvatags)
-#    SetObjectStyle(histo1,color=kAzure+3,fillalpha=0.5,linewidth=1)
-#    histo1.Draw("hist,same")
-#    SetObjectStyle(histo2,color=kOrange-3,fillalpha=0.9,linewidth=1)
-#    histo2.Draw("hist,same")
-#    legend.Draw("same")
 
 if __name__ == '__main__':
     infilenames= ['data/input/Diamond/TimeEv/30lun1.mca','data/input/Diamond/TimeEv/30lun2.mca',
